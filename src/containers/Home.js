49ad4f8fd6e58b3c0056b9122d824eb6dbbdb6ac
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-12 14:05:36 +0100

    get data from api leboncoin

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get("https://le-bon-coin.herokuapp.com/");
+    console.log("data", response);
+  };
   return (
     <div>
       Home
