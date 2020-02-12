aed5598b31db5d935894b2473d889dfcace2360d
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-12 13:04:44 +0100

    react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./containers/Home";
+import Offer from "./containers/Offer";
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/offer">
+          <Offer />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/containers/Offer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Offer = () => {
+  return (
+    <div>
+      <Link to="/">hOME</Link>
+      Offer
+    </div>
+  );
+};
+
+export default Offer;
