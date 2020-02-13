140c01762b2d19b63394dff55282b512ce3a71ac
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-13 15:40:48 +0100

    formulaire signup avec axios

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,21 +1,66 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordConfirm, setpasswordConfirm] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const body = {
+      username: username,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword
+    };
+    console.log(username, email, password, confirmPassword);
+    const response = await axios.post(
+      "http://localhost:3000/user/signup",
+      body
+    );
+    console.log(response.data);
+  };
 
   return (
     <div style={{ width: 300, height: 300 }}>
       <div>
         <h2>Créez un compte</h2>
       </div>
-      <form>
-        <input type="text" placeholder="username" />
-        <input type="email" placeholder="adresse email" />
-        <input type="password" placeholder="adresse email" />
-        <input type="password" placeholder="confirm password" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          placeholder="username"
+          onChange={event => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="email"
+          value={email}
+          placeholder="adresse email"
+          onChange={event => {
+            setEmail(event.target.value);
+          }}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={event => {
+            setPassword(event.target.value);
+          }}
+        />
+        <input
+          type="password"
+          value={confirmPassword}
+          placeholder="confirm password"
+          onChange={event => {
+            setConfirmPassword(event.target.value);
+          }}
+        />
         <button>Submit</button>
       </form>
     </div>
